58a3c0cf669e7eaa7d67d93610190a0eaa3a6ebe
Benjamin Sonntag <benjamin@octopuce.fr>
2024-02-21 12:49:49 +0100

    [dev] now depending on php8.2+, fixing totp dependency, fixing typos in last commit

--- pub/message.php
+++ pub/message.php
@@ -1,10 +1,10 @@
-<?php if ($error) {
+<?php if (isset($error) && $error) {
 ?>
 <div class="alert alert-danger" role="alert"><?php echo $error; ?></div>
 <?php
 }
 ?>
-<?php if ($info) {
+<?php if (isset($info) && $info) {
 ?>
 <div class="alert alert-info" role="alert"><?php echo $info; ?></div>
 <?php
