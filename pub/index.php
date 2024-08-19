ea9579918641eb40ff9002eb49dfe43dad2ee681
Benjamin Sonntag <benjamin@octopuce.fr>
2024-08-19 19:10:45 +0200

    [fix] change the username regexp for search too

--- pub/index.php
+++ pub/index.php
@@ -8,7 +8,7 @@ $sql="";
 $ss="";
 $search="";
 if (isset($_GET["s"])) {
-    $search=trim(preg_replace("#[^a-z0-9-]#","",strtolower($_GET["s"])));
+    $search=trim(preg_replace("#[^a-z0-9@\.-]#","",strtolower($_GET["s"])));
     if ($search) {
         $sql.=" AND username like '%".addslashes($search)."%'";
         $ss.="&s=".urlencode($search);
